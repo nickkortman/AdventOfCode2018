456b3fe2d1e2e02150626fea72b62cbb6fca6131
Nick Kortman <nakort@umich.edu>
2018-12-01 12:15:39 -0800

    fixed tabbing

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,36 +3,36 @@ use std::collections::HashSet;
 
 fn change_frequency(change: &String, frequency: &mut i128)
 {
-	let (sign, num) = change.split_at(1);
-	let num_val = num.parse::<i128>();
-	match num_val
-	{
-		Ok(value) =>
-		{
-			if sign == "+"
-			{
-				*frequency += value;
-			}
-			else
-			{
-				*frequency -= value;
-			}
-		}
-		Err(error) => println!("Parse Error: {}", error),
-	}
+    let (sign, num) = change.split_at(1);
+    let num_val = num.parse::<i128>();
+    match num_val
+    {
+        Ok(value) =>
+        {
+            if sign == "+"
+            {
+                *frequency += value;
+            }
+            else
+            {
+                *frequency -= value;
+            }
+        }
+        Err(error) => println!("Parse Error: {}", error),
+    }
 }
 
 fn monitor_frequency(change: &String, frequency: &mut i128, seen_frequencies: &mut HashSet<i128>) -> bool
 {
-	change_frequency(change, frequency);
+    change_frequency(change, frequency);
 
-	if seen_frequencies.contains(&*frequency)
-	{
-		println!("Repeated frequency: {}", *frequency);
-		return true;
-	}
-	
-	seen_frequencies.insert(*frequency);
+    if seen_frequencies.contains(&*frequency)
+    {
+        println!("Repeated frequency: {}", *frequency);
+        return true;
+    }
+    
+    seen_frequencies.insert(*frequency);
     false
 }
 
@@ -40,34 +40,34 @@ fn main()
 {
     let mut frequency = 0i128;
     let mut seen_frequencies: HashSet<i128> = HashSet::new();
-	let mut changes: Vec<String> = Vec::new();
+    let mut changes: Vec<String> = Vec::new();
     let stdin = io::stdin();
-	let input = stdin.lock();
+    let input = stdin.lock();
 
     for line in input.lines()
     {
-		match line
-		{
-        	Ok(change) => 
-			{
-				if monitor_frequency(&change, &mut frequency, &mut seen_frequencies)
+        match line
+        {
+            Ok(change) => 
+            {
+                if monitor_frequency(&change, &mut frequency, &mut seen_frequencies)
                 {
                     return;
                 }
-				changes.push(change);
-			}
-			Err(error) => println!("IO Error: {}", error),
-		}
+                changes.push(change);
+            }
+            Err(error) => println!("IO Error: {}", error),
+        }
     }
-	
-	loop
-	{
-		for change in &changes
-		{
-			if monitor_frequency(&change, &mut frequency, &mut seen_frequencies)
-			{
-				return;
-			}
-		}
-	}
+    
+    loop
+    {
+        for change in &changes
+        {
+            if monitor_frequency(&change, &mut frequency, &mut seen_frequencies)
+            {
+                return;
+            }
+        }
+    }
 }
